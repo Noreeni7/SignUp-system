e6b59cbb7dc25edbf8cd32338bdedf88284952ea
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2025-10-09 15:43:35 +0300

    Add PDO database connection setup with error handling

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,2 +1,13 @@
 
 <?php
+$servername = 'localhost';
+$dbname = 'signup_system_database';
+$dbusername = 'root';
+$dbpassword ='';
+
+try {
+    $pdo = new PDO ("mysql:host=$host;dbname=$dbname",$dbusername,$dbpassword);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
